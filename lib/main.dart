b82d8613a7d64ed248e6e987993a4d05b682681a
vmoskvyak <volodymyr.moskvyack@valtech.com>
2020-06-03 10:13:28 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
-    
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
