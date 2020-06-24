442fb0c6aa59bba1351abce143848adb5a65a462
vmoskvyak <volodymyr.moskvyack@valtech.com>
2020-06-24 09:32:45 +0200

    create note repo

--- lib/injection.iconfig.dart
+++ lib/injection.iconfig.dart
@@ -9,8 +9,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:ddd_architecture/infrastructure/auth/firebase_auth_facade.dart';
 import 'package:ddd_architecture/domain/auth/i_auth_facade.dart';
-import 'package:ddd_architecture/application/notes/note_actor/note_actor_bloc.dart';
+import 'package:ddd_architecture/infrastructure/notes/note_repository.dart';
 import 'package:ddd_architecture/domain/notes/repository/i_note_repository.dart';
+import 'package:ddd_architecture/application/notes/note_actor/note_actor_bloc.dart';
 import 'package:ddd_architecture/application/notes/note_watcher/note_watcher_bloc.dart';
 import 'package:ddd_architecture/application/auth/sign_in_form/sign_in_form_bloc.dart';
 import 'package:ddd_architecture/application/auth/auth_bloc.dart';
@@ -24,6 +25,7 @@ void $initGetIt(GetIt g, {String environment}) {
       () => firebaseInjectableModule.googleSignIn);
   g.registerLazySingleton<IAuthFacade>(
       () => FirebaseAuthFacade(g<FirebaseAuth>(), g<GoogleSignIn>()));
+  g.registerLazySingleton<INoteRepository>(() => NoteRepository(g()));
   g.registerFactory<NoteActorBloc>(() => NoteActorBloc(g<INoteRepository>()));
   g.registerFactory<NoteWatcherBloc>(
       () => NoteWatcherBloc(g<INoteRepository>()));
