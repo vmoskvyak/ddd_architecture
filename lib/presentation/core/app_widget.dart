0afeb361936d4e3765bdd452706fbbc3ab18ad7f
vmoskvyak <volodymyr.moskvyack@valtech.com>
2020-08-07 15:54:25 +0200

    implement notes overview page

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:ddd_architecture/application/auth/auth_bloc.dart';
 import 'package:ddd_architecture/injection.dart';
-import 'package:ddd_architecture/presentation/routes/router.gr.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
